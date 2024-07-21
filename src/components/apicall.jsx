93bb7e9bda990bb63998605986800478bd7bc4cf
Damini Rathor <daminirathor8@gmail.com>
2024-07-21 20:34:58 +0530

    Update apicall.jsx

--- src/components/apicall.jsx
+++ src/components/apicall.jsx
@@ -1,5 +1,5 @@
 // const Recipefinder=async(query)=>{
-//     const response=await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=abab7309fab248728a9ff5279394bf91`)
+//     const response=await fetch(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=Your Access Key`)
 //     const data= await response.json();
 //     return data;
 
